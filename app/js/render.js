25813efc080b0eb066c382d72777cd2c253fa972
Estevao <estevaovasques@hotmail.com>
2019-04-15 20:32:51 -0300

    Primeiro commit

---
+++ app/js/render.js
@@ -0,0 +1,8 @@
+const { ipcRenderer } = require('electron');
+
+let linkSobre = document.querySelector('#link-sobre');
+
+linkSobre
+    .addEventListener('click', () => {
+        ipcRenderer.send('abrir-janela-sobre');
+    });
